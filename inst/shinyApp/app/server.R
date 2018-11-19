8900fdf2b4a3944481fd7be4263ba125eaed81ee
IsamBenS <isam.ben-soltane@inserm.fr>
2018-11-19 15:12:00 +0100

    Revert "v1.3"
    This reverts commit f7938274937ffbb00ad180c15d9d65769c5fd053.

--- inst/shinyApp/app/server.R
+++ inst/shinyApp/app/server.R
@@ -5,17 +5,6 @@ library(flowCore)
 library(doSNOW)
 library(parallel)
 
-source("../EfficiencyAnalysis.R")
-source("../FilePreHandling.R")
-source("../MethodsHandling.R")
-source("../ModifyFCS.R")
-source("../StatisticalAnalysis.R")
-source("../TimeAnalysis.R")
-source("../Runsfunctions.R")
-source("../PlotsFunctions.R")
-source("../Preprocessing.R")
-source("../RAMConsumption.R")
-
 
 
 server <- function(input, output, session)
@@ -37,8 +26,7 @@ server <- function(input, output, session)
     
     clustering.algorithms <- reactiveValues(
         algorithms = NULL,
-        parameters = NULL,
-        parameters.description = NULL
+        parameters = NULL
     )
     
     env.var <- reactiveValues(
@@ -766,10 +754,6 @@ server <- function(input, output, session)
             {
                 clustering.algorithms$parameters <- list()
             }
-            if( is.null(clustering.algorithms$parameters.description) )
-            {
-                clustering.algorithms$parameters.description <- list()
-            }
 
             lapply(methods.files, function(f)
             {
@@ -777,7 +761,6 @@ server <- function(input, output, session)
 
                 clustering.algorithms$algorithms[[strsplit(f,".R", fixed = T)[[1]][1]]] <<- strsplit(f,".R", fixed = T)[[1]][1]
                 clustering.algorithms$parameters[[strsplit(f,".R", fixed = T)[[1]][1]]] <<- fct.parameters
-                clustering.algorithms$parameters.description[[strsplit(f,".R", fixed = T)[[1]][1]]] <<- fct.parameters.description
             })
         }
 
@@ -828,18 +811,12 @@ server <- function(input, output, session)
                                      (
                                          sliderInput(paste0("t_2_3_",k,"_",p),par.name,min = as.numeric(par[1]),max=as.numeric(par[3]),
                                                      step=as.numeric(par[2]),value=c(as.numeric(par[4]),as.numeric(par[4]))),
-                                         style="float:left;width:65%;margin-bottom:11vh"
+                                         style="float:left;width:80%;"
                                      ),
                                      div
                                      (
                                          textInput(paste0("t_2_3_",k,"_",p,"_step"), "Step", value=as.numeric(par[2])),
                                          style="float:left;width:10%;margin-top:2vh;margin-left:1.2vw"
-                                     ),
-                                     div
-                                     (
-                                         p(clustering.algorithms$parameters.description[[input$t_2_1_sel[[k]]]][[p]]),
-                                         style="float:left;width:20%;margin-top:2vh;margin-left:1.2vw;max-height:10vh;
-                                                overflow:auto;padding-top:2vh"
                                      )
                                  )
                         )
